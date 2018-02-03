1fc7d8ae8fe45eeaedf4b9ab68c55f9e238c4f8a
Victor <ivictormail@gmail.com>
2018-02-03 19:31:29 +0300

    second branch (refactoring)

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -10,54 +10,16 @@ import Foundation
 
     struct CalculatorBrain {
     
-    private var accumulator: Double?
-        
-    private var displayString: String? {
-        get {
-            if accumulator != nil {
-                switch accumulator! {
-                    case Double.pi:
-                        return "π"
-                    case M_E:
-                        return "e"
-                    default:
-                        return accumulator!.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", accumulator!) : String(accumulator!)
-                }
-            }
-            else {
-                return nil
-            }
-        }
-    }
-    
-    public var resultIsPending: Bool = false
+    private var accumulator: (value: Double?, displayValue: String) = (nil, "")
     
     public var description: String?
         
-    private var descPart2: String = ""
-        
-    public var comboOperation = false
-        
     private var pendingBinaryOperation: PendingBinaryOperation?
-        
-    public var pendingComboOperation: PendingComboOperation?
-        
-    private var resultIsFixed = false
-        
-    public var lastOperation: String?
-    
-    private var lastOperationType: String?
-        
-    private var lastBinaryOperation: String?
-        
-    private let highPrioretyOperations = ["x", "/"]
-        
-    private let lowPrioretyOperations = ["+", "-"]
         
     private enum Operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
-        case binaryOperation((Double, Double) -> Double)
+        case binaryOperation((Double, Double) -> Double, Int)
         case equals()
         case clear()
     }
@@ -74,146 +36,78 @@ import Foundation
     "x²": Operation.unaryOperation({pow($0, 2)}),
     "x³": Operation.unaryOperation({pow($0, 3)}),
     "±": Operation.unaryOperation({-$0}),
-    "+": Operation.binaryOperation({$0 + $1}),
-    "-": Operation.binaryOperation({$0 - $1}),
-    "x": Operation.binaryOperation({$0 * $1}),
-    "/": Operation.binaryOperation({$0 / $1}),
+    "+": Operation.binaryOperation({$0 + $1}, 0),
+    "-": Operation.binaryOperation({$0 - $1}, 0),
+    "x": Operation.binaryOperation({$0 * $1}, 1),
+    "/": Operation.binaryOperation({$0 / $1}, 1),
     //"log": Operation.binaryOperation({log($0) / log($1)}),
-    "xʸ": Operation.binaryOperation({pow($0, $1)}),
+    "xʸ": Operation.binaryOperation({pow($0, $1)}, 2),
     "=": Operation.equals(),
     "C": Operation.clear()
     ]
 
-    
+    mutating func setAccumulator(value newValue: Double, displayValue: String?) {
+        accumulator.value = newValue
+        if(displayValue != nil){
+                accumulator.displayValue = displayValue!
+        }
+        else {
+        accumulator.displayValue = newValue.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", newValue) : String(newValue)
+        }
+    }
+        
     mutating func performOperation(_ symbol: String){
+        
         if let operation = operations[symbol]{
             switch operation {
             case .constant(let value):
-                accumulator = value
-                if resultIsPending {
-                    if pendingComboOperation != nil {
-                        pendingComboOperation!.descPart2 = symbol
-                    }
-                }
-                else {
-                    if description != nil {
-                        description = symbol
-                    }
+                setAccumulator(value: value, displayValue: symbol)
+                if resultIsPending == false {
+                    description = accumulator.displayValue
                 }
             case .unaryOperation(let function):
-                
-                if(resultIsPending) {
-                    
-                    comboOperation = true // binary + unary (unary operation under 2nd operand of binary operation)
-                    
-                    if pendingComboOperation == nil {
-                        if(accumulator != nil){
-                            descPart2 = symbol + addBrackets(to: displayString!)
-                        }
-                        else {
-                            descPart2 = ""
-                        }
-                         print("{1}")
-                    }
-                    else {
-                        if lastOperationType == "unary" {
-                            descPart2 = symbol + addBrackets(to: pendingComboOperation!.descPart2)
-                        }
-                        else {
-                             descPart2 = symbol + addBrackets(to: displayString!)
-                        }
-                        
-                        print("{3}")
-                    }
-                    
-                    print("\(description!), \(descPart2)")
-                    
-                   
-                    pendingComboOperation = PendingComboOperation(descPart1: description!, descPart2: descPart2)
-                    
-                 
-                    
-                    print("\(pendingComboOperation!.descPart1), \(pendingComboOperation!.descPart2)")
-                }
-                
-                else {
-                   if accumulator != nil {
-                        if description == nil {
-                           description =  symbol + addBrackets(to: displayString!)
-                        }
-                        else {
-                             description = symbol + addBrackets(to: (description!))
-                        }
-                    }
-                }
-                
-                if accumulator != nil {
-                    accumulator = function(accumulator!)
-                }
-              
-                lastOperationType = "unary"
-                
-            case .binaryOperation(let function):
-                
-                if pendingBinaryOperation != nil && resultIsPending {
-                    performPendingBinaryOperation() // 2 step of binary operation
-                }
-                
-                /* 1 step of binary operation: save function and first operand: */
+                if accumulator.value != nil {
+                    setAccumulator(value: function(accumulator.value!), displayValue: symbol + addBrackets(to: accumulator.displayValue))
 
-                if accumulator != nil {
-                pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)                 }
-                
-                if  lastBinaryOperation != nil && description != nil {  // add brackets to description if last operation have low priority
-                    if lowPrioretyOperations.contains(lastBinaryOperation!) && highPrioretyOperations.contains(symbol) {
-                    description = addBrackets(to: description!)
+                    if resultIsPending == false {
+                        description = accumulator.displayValue
                     }
+ 
                 }
-                
-                if description == nil {
-                    description = displayString! + symbol
+            case .binaryOperation(let function, let priority):
+                if accumulator.value != nil {
+        
+                    if resultIsPending == false {
+                        description = accumulator.displayValue
+                    }
+                    
+                    if pendingBinaryOperation != nil {
+                        performPendingBinaryOperation()
+                        
+                        if priority > pendingBinaryOperation!.priority {
+                            description = addBrackets(to: description!)
+                        }
+                    }
+                    
+                    description! += symbol
+                    
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator.value!, priority: priority, resultIsPending: true)
+                    
                 }
-                else {
-                    description = description! + symbol
-                }
-                
-                comboOperation = false
-                resultIsPending = true
-                lastOperationType = "binary"
-                lastBinaryOperation = symbol
-           
             case .equals:
-                if lastOperation == "=" {
-                    break
-                }
-                if lastOperation == "unary" || resultIsPending == false {
-                    break
-                }
                 performPendingBinaryOperation()
-                resultIsPending = false
-                comboOperation = false
-                resultIsFixed = true
-                pendingComboOperation = nil
-                
+                if pendingBinaryOperation != nil {
+                    pendingBinaryOperation!.resultIsPending = false
+                }
             case .clear:
-                accumulator = nil
+                accumulator = (nil, "")
                 description = nil
-                resultIsPending = false
                 pendingBinaryOperation = nil
-                comboOperation = false
-                resultIsFixed = false
-                lastOperation = nil
-                lastOperationType = nil
-                pendingComboOperation = nil
             }
             
-            lastOperation = symbol
         }
         
-        if description != nil {
-            print("desc: \(description!)")
         }
-    }
     
     private func addBrackets(to operand: String) -> String {
         return "(" + operand + ")"
@@ -222,45 +116,44 @@ import Foundation
     private struct PendingBinaryOperation {
             let function: (Double, Double) -> Double
             let firstOperand: Double
-            
+            let priority: Int
+            var resultIsPending: Bool
+        
             func perform(with secondOperand: Double) -> Double {
                 return function(firstOperand, secondOperand)
             }
         }
-        
-    public struct PendingComboOperation {
-            var descPart1: String
-            var descPart2: String
-            mutating func performDescription() -> String {
-                return descPart1 + descPart2
-            }
-        }
-        
+    
     
     private mutating func performPendingBinaryOperation(){ // if press equals or binary operation (when pending)
         
-     if(pendingBinaryOperation != nil && accumulator != nil && description != nil ) {
-        
-            if comboOperation == false  {
-                description = description! + displayString!
+     if(pendingBinaryOperation != nil && accumulator.value != nil ) {
+        if pendingBinaryOperation!.resultIsPending == true {
+            description! += accumulator.displayValue
+            setAccumulator(value: pendingBinaryOperation!.perform(with: accumulator.value!), displayValue: description!)
             }
-            else {
-                if pendingComboOperation != nil {
-                    description = pendingComboOperation!.performDescription() // merge parts of description after combo operation is done
-                }
-            }
- 
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
         }
     }
     
     mutating func setOperand(_ operand: Double) {
-        accumulator = operand
+        setAccumulator(value: operand, displayValue: nil)
     }
     
-    var result: Double? {
+    var result: (Double?) {
         get {
-            return accumulator
+            return accumulator.value
         }
     }
+        
+        var resultIsPending: Bool {
+            get {
+                if pendingBinaryOperation != nil {
+                  return pendingBinaryOperation!.resultIsPending
+                }
+                else {
+                    return false
+                }
+            }
+        }
+
 }
